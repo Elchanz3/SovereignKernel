b7277a28d19ad26e31a4ae626d78c53f969d6747
snapdragon teaks <inutilidades639@gmail.com>
2023-11-26 04:53:18 -0300

    update

--- build.sh
+++ build.sh
@@ -1,218 +1,25 @@
 #!/bin/bash
-# GrifoDev script
 
-export MODEL=greatlte
-export VARIANT=eur
-export ARCH=arm64
-export BUILD_CROSS_COMPILE=../Toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+mkdir out
 
-RDIR=$(pwd)
-OUTDIR=$RDIR/arch/$ARCH/boot
-DTSDIR=$RDIR/arch/$ARCH/boot/dts/exynos
-DTBDIR=$OUTDIR/dtb
-DTCTOOL=$RDIR/scripts/dtc/dtc
-INCDIR=$RDIR/include
+export ANDROID_MAJOR_VERSION=8
+export ANDROID_PLATFORM_VERSION=80000
 
-PAGE_SIZE=2048
-DTB_PADDING=0
+make O=out ARCH=arm64 exynos8895-greatlte_defconfig
 
-case $MODEL in
-greatlte)
-	case $VARIANT in
-	can|duos|eur|xx)
-		KERNEL_DEFCONFIG=exynos8895-greatlte_defconfig
-		;;
-	*)
-		echo "Unknown variant: $VARIANT"
-		exit 1
-		;;
-	esac
-;;
-*)
-	echo "Unknown device: $MODEL"
-	exit 1
-	;;
-esac
+DATE_START=$(date +"%s")
 
-FUNC_CLEAN_DTB()
-{
-	if ! [ -d $RDIR/arch/$ARCH/boot/dts ] ; then
-		echo "no directory : "$RDIR/arch/$ARCH/boot/dts""
-	else
-		echo "rm files in : "$RDIR/arch/$ARCH/boot/dts/*.dtb""
-		rm $RDIR/arch/$ARCH/boot/dts/*.dtb
-		rm $RDIR/arch/$ARCH/boot/dtb/*.dtb
-		rm $RDIR/arch/$ARCH/boot/boot.img-dtb
-		rm $RDIR/arch/$ARCH/boot/boot.img-zImage
-	fi
-}
+make O=out ARCH=arm64 -j12
 
-FUNC_BUILD_DTIMAGE_TARGET()
-{
-	[ -f "$DTCTOOL" ] || {
-		echo "You need to run ./build.sh first!"
-		exit 1
-	}
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
 
-	case $MODEL in
-	greatlte)
-		case $VARIANT in
-		can|duos|eur|xx)
-			DTSFILES="exynos8895-greatlte_eur_open_00 exynos8895-greatlte_eur_open_01
-					exynos8895-greatlte_eur_open_02 exynos8895-greatlte_eur_open_06"
-			;;
-		*)
-			echo "Unknown variant: $VARIANT"
-			exit 1
-			;;
-		esac
-	;;
-	*)
-		echo "Unknown device: $MODEL"
-		exit 1
-		;;
-	esac
-
-	mkdir -p $OUTDIR $DTBDIR
-
-	cd $DTBDIR || {
-		echo "Unable to cd to $DTBDIR!"
-		exit 1
-	}
-
-	rm -f ./*
-
-	echo "Processing dts files..."
-
-	for dts in $DTSFILES; do
-		echo "=> Processing: ${dts}.dts"
-		${CROSS_COMPILE}cpp -nostdinc -undef -x assembler-with-cpp -I "$INCDIR" "$DTSDIR/${dts}.dts" > "${dts}.dts"
-		echo "=> Generating: ${dts}.dtb"
-		$DTCTOOL -p $DTB_PADDING -i "$DTSDIR" -O dtb -o "${dts}.dtb" "${dts}.dts"
-	done
-
-	echo "Generating dtb.img..."
-	$RDIR/scripts/dtbTool/dtbTool -o "$OUTDIR/dtb.img" -d "$DTBDIR/" -s $PAGE_SIZE
-
-	echo "Done."
-}
-
-FUNC_BUILD_KERNEL()
-{
-	echo ""
-        echo "=============================================="
-        echo "START : FUNC_BUILD_KERNEL"
-        echo "=============================================="
-        echo ""
-        echo "build common config="$KERNEL_DEFCONFIG ""
-        echo "build model config="$MODEL ""
-
-	FUNC_CLEAN_DTB
-
-	make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
-			CROSS_COMPILE=$BUILD_CROSS_COMPILE \
-			$KERNEL_DEFCONFIG || exit -1
-
-	make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
-			CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
-
-	FUNC_BUILD_DTIMAGE_TARGET
-	
-	echo ""
-	echo "================================="
-	echo "END   : FUNC_BUILD_KERNEL"
-	echo "================================="
-	echo ""
-}
-
-FUNC_BUILD_RAMDISK()
-{
-	mv $RDIR/arch/$ARCH/boot/Image $RDIR/arch/$ARCH/boot/boot.img-zImage
-	mv $RDIR/arch/$ARCH/boot/dtb.img $RDIR/arch/$ARCH/boot/boot.img-dtb
-
-	case $MODEL in
-	greatlte)
-		case $VARIANT in
-		can|duos|eur|xx)
-			rm -f $RDIR/ramdisk/N950F/split_img/boot.img-zImage
-			rm -f $RDIR/ramdisk/N950F/split_img/boot.img-dtb
-			mv -f $RDIR/arch/$ARCH/boot/boot.img-zImage $RDIR/ramdisk/N950F/split_img/boot.img-zImage
-			mv -f $RDIR/arch/$ARCH/boot/boot.img-dtb $RDIR/ramdisk/N950F/split_img/boot.img-dtb
-			cd $RDIR/ramdisk/N950F
-			./repackimg.sh --nosudo
-			echo SEANDROIDENFORCE >> image-new.img
-			;;
-		*)
-			echo "Unknown variant: $VARIANT"
-			exit 1
-			;;
-		esac
-	;;
-	*)
-		echo "Unknown device: $MODEL"
-		exit 1
-		;;
-	esac
-}
-
-FUNC_BUILD_ZIP()
-{
-	cd $RDIR/build
-	rm $MODEL-$VARIANT.img
-	case $MODEL in
-	dreamlte)
-		case $VARIANT in
-		can|duos|eur|xx)
-			mv -f $RDIR/ramdisk/G950F/image-new.img $RDIR/build/$MODEL-$VARIANT.img
-			;;
-		*)
-			echo "Unknown variant: $VARIANT"
-			exit 1
-			;;
-		esac
-	;;
-	dream2lte)
-		case $VARIANT in
-		can|duos|eur|xx)
-			mv -f $RDIR/ramdisk/G955F/image-new.img $RDIR/build/$MODEL-$VARIANT.img
-			;;
-		*)
-			echo "Unknown variant: $VARIANT"
-			exit 1
-			;;
-		esac
-	;;
-	greatlte)
-		case $VARIANT in
-		can|duos|eur|xx)
-			mv -f $RDIR/ramdisk/N950F/image-new.img $RDIR/build/$MODEL-$VARIANT.img
-			;;
-		*)
-			echo "Unknown variant: $VARIANT"
-			exit 1
-			;;
-		esac
-	;;
-	*)
-		echo "Unknown device: $MODEL"
-		exit 1
-		;;
-	esac
-}
-
-# MAIN FUNCTION
-rm -rf ./build.log
-(
-	START_TIME=`date +%s`
-
-	FUNC_BUILD_KERNEL
-	FUNC_BUILD_RAMDISK
-	FUNC_BUILD_ZIP
-
-	END_TIME=`date +%s`
-	
-	let "ELAPSED_TIME=$END_TIME-$START_TIME"
-	echo "Total compile time was $ELAPSED_TIME seconds"
-
-) 2>&1	 | tee -a ./build.log
+IMAGE="out/arch/arm64/boot/Image"
+if [[ -f "$IMAGE" ]]; then
+	rm AnyKernel3/zImage > /dev/null 2>&1
+	rm AnyKernel3/dtb > /dev/null 2>&1
+	rm AnyKernel3/*.zip > /dev/null 2>&1
+	mv $IMAGE AnyKernel3/zImage
+	cd AnyKernel3
+	zip -r9 SovereignKernel-N950F-$(date +"%Y%m%d%H%M")-beta_aosp.zip .
+fi
